0125b1ef04e5e8145c79c65b101398fa43db7b47
ShubhamKryadav <141262209+ShubhamKryadav@users.noreply.github.com>
2023-08-04 12:45:29 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch23 team");
+     System.out.println("Hello shubham 8888888888888888888888");
       System.out.println("Hello Batch batch23 WITH FOR STUDENTS RUNNING in front of ayesha and saddam and and prashant naveen doining the CI and 1 changeconfiguration ");
     }  
 } 
